0b2124c4f567943cb060c6628786d5ab028c4058
skanon <nosferatu.alucard@hotmail.com>
2014-02-19 10:47:32 +0100

    U1 A1 complete

--- src/skanon/uebung01/aufgabe01/InetAddressTest.java
+++ src/skanon/uebung01/aufgabe01/InetAddressTest.java
@@ -3,13 +3,20 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 
-public class UriTester {
+public class InetAddressTest {
 
 	
 	public static void main(String[] args) throws UnknownHostException {
-		
 		System.out.println("a) " + InetAddress.getLocalHost());
 		System.out.println("b) " + InetAddress.getByName("sidv0012.hsr.ch").getHostAddress());
+		System.out.println("c) " + InetAddress.getByName("152.96.36.121").getHostName());
+		System.out.println("d) google IPs:");
+		InetAddress[] adresses = InetAddress.getAllByName("www.google.com");
+		
+		for (InetAddress adr : adresses) {
+			System.out.println(adr.getHostAddress());
+		}
+		
 		
 	}
 }
